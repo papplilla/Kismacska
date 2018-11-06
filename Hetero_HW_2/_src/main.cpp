9b1cb6ba0ae421ffcff1473ca2efc58fc6133225
Lilla PAPP <papp.lilla9c@gmail.com>
2018-11-06 16:56:29 +0100

    It is a new day!

---
+++ Hetero_HW_2/_src/main.cpp
@@ -0,0 +1,170 @@
+// lab1.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include "memory.h"
+#include "time.h"
+
+#include "omp.h"
+
+#include <IL/ilut.h>
+#include <IL/ilu.h>
+
+#include "emmintrin.h"
+#include "nmmintrin.h"
+
+#include "defs.h"
+#include "func.h"
+
+
+void main()
+{
+	ilInit(); iluInit();
+	ILboolean ret;
+	ILuint ilImg=0;
+	ilGenImages(1, &ilImg);
+	ilBindImage(ilImg);
+    ret = ilLoadImage((const char*)("input.jpg"));
+	ILubyte* imgData = ilGetData(); 
+
+	int imgWidth = ilGetInteger(IL_IMAGE_WIDTH);
+	int imgHeight = ilGetInteger(IL_IMAGE_HEIGHT);
+	ILint imgOrigin = ilGetInteger(IL_ORIGIN_MODE);
+
+	printf("Input resolution: %4dx%4d\n", imgWidth, imgHeight);
+
+	unsigned char *imgSrcExt;
+	int imgWidthF = imgWidth+FILTER_W-1;
+	int imgHeightF = imgHeight+FILTER_H-1;
+	int imgFOfssetW = (FILTER_W-1)/2;
+	int imgFOfssetH = (FILTER_H-1)/2;
+	imgSrcExt = (unsigned char *)(_aligned_malloc(3*imgWidthF*imgHeightF*sizeof(unsigned char), 32));
+    int row, col;
+
+	for (row=0; row<imgHeightF; row++)
+	{
+		for (col=0; col<imgWidthF;col++)
+		{
+			int pixel = (row*imgWidthF + col)*3;
+			*(imgSrcExt + pixel + 0) = 0;
+			*(imgSrcExt + pixel + 1) = 0;
+			*(imgSrcExt + pixel + 2) = 0;
+		}
+	}
+
+	for (row=0; row<imgHeight; row++)
+	{
+		for (col=0; col<imgWidth;col++)
+		{
+			int pixel_dst = ((row+imgFOfssetH)*imgWidthF + (col+imgFOfssetW))*3;
+			int pixel_src = (row*imgWidth + col)*3;
+			*(imgSrcExt + pixel_dst + 0) = (unsigned char)(*(imgData + pixel_src + 0));
+			*(imgSrcExt + pixel_dst + 1) = (unsigned char)(*(imgData + pixel_src + 1));
+			*(imgSrcExt + pixel_dst + 2) = (unsigned char)(*(imgData + pixel_src + 2));
+		}
+	}
+
+	unsigned char *imgRes;
+	imgRes = (unsigned char *)(_aligned_malloc(3 * imgWidth*imgHeight * sizeof(unsigned char), 32));
+	
+
+// IMAGE PROCESSING
+//---------------------------------------------------------------------------------------
+	clock_t s0, e0;
+    double d0;
+
+	//short filter_laplace[] = {-1, -1, -1, -1, -1,
+	//                          -1, -1, -1, -1, -1,
+	//                          -1, -1, 24, -1, -1,
+	//	                      -1, -1, -1, -1, -1,
+	//	                      -1, -1, -1, -1, -1};
+	double mpixel;
+
+	short *imgDstConv;
+	imgDstConv = (short*)(_aligned_malloc(3 * imgWidthF*imgHeightF * sizeof(short), 32));
+
+	printf("Start convolution\n");
+	s0 = clock();
+for (int r=0; r<RUNS; r++)
+{
+#if 1
+	median_filter(imgHeight, imgWidth, imgHeightF, imgWidthF, imgSrcExt, imgRes);
+#endif
+
+#if 0
+    conv_filter_avx(imgHeight, imgWidth, imgWidthF,
+				filter_laplace, imgSrcExt, imgRes);
+#endif
+}
+
+    e0 = clock();
+    d0 = (double)(e0-s0)/(RUNS*CLOCKS_PER_SEC);
+	mpixel = (imgWidth*imgHeight/d0)/1000000;
+    printf("C CPU TIME: %4.4f\n", d0);
+	printf("C Mpixel/s: %4.4f\n", mpixel);
+
+
+
+	    s0 = clock();
+for (int r=0; r<RUNS; r++)
+{
+/*
+	conv_filter_sse(imgHeight, imgWidth, imgHeightF, imgWidthF,
+				imgFOfssetH, imgFOfssetW,
+				filter_laplace, imgFloat, imgFloatRes);
+	
+	conv_filter(imgHeight, imgWidth, imgHeightF, imgWidthF,
+		imgFOfssetH, imgFOfssetW,
+		filter_laplace, imgFloat, imgFloatRes);
+
+	conv_filter_block_sse(imgHeight, imgWidth, imgHeightF, imgWidthF,
+		imgFOfssetH, imgFOfssetW,
+		filter_laplace, imgFloat, imgFloatRes);
+*/
+}
+    e0 = clock();
+    d0 = (double)(e0-s0)/(RUNS*CLOCKS_PER_SEC);
+	mpixel = (imgWidth*imgHeight/d0)/1000000;
+    printf("SSE CPU TIME: %4.4f\n", d0*1000);
+	printf("SSE Mpixel/s: %4.4f\n", mpixel);
+
+/*
+    s0 = clock();
+for (int r=0; r<RUNS; r++)
+{
+    conv_filter_asm(imgHeight, imgWidth, imgHeightF, imgWidthF,
+				imgFOfssetH, imgFOfssetW,
+				filter_laplace, imgFloat, imgFloatRes);
+}
+    e0 = clock();
+    d0 = (double)(e0-s0)/(RUNS*CLOCKS_PER_SEC);
+	mpixel = (imgWidth*imgHeight/d0)/1000000;
+	printf("ASM CPU TIME: %4.4f\n", d0);
+	printf("ASM Mpixel/s: %4.4f\n", mpixel);
+*/
+
+//---------------------------------------------------------------------------------------
+// IMAGE PROCESSING END
+
+	for (row=0; row<imgHeight; row++)
+	{
+		for (col=0; col<imgWidth;col++)
+		{
+			int pixel_src = (row*imgWidth + col)*3;
+			int pixel_dst = (row*imgWidth + col)*3;
+			*(imgData + pixel_dst + 0) = (ILubyte)(*(imgRes + pixel_src + 0));
+			*(imgData + pixel_dst + 1) = (ILubyte)(*(imgRes + pixel_src + 1));
+			*(imgData + pixel_dst + 2) = (ILubyte)(*(imgRes + pixel_src + 2));
+		}
+	}
+
+	_aligned_free(imgDstConv);
+	_aligned_free(imgSrcExt);
+    _aligned_free(imgRes);
+
+	ret = ilSetData(imgData);
+	ilEnable(IL_FILE_OVERWRITE);
+    ilSaveImage((const char*)("output.jpg"));
+	ilDeleteImages(1, &ilImg);
+}
+
---
+++ Hetero_HW_2/_src/median_filter.cpp
@@ -0,0 +1,112 @@
+﻿#include "omp.h"
+#include "emmintrin.h"
+#include "nmmintrin.h"
+#include "defs.h"
+#include "func.h"
+
+#include <stdio.h>
+
+/*void median_filter(int imgHeight, int imgWidth, int imgHeightF, int imgWidthF, unsigned char *imgSrcExt, unsigned char *imgDst)
+{
+	printf("Median filter called\n");
+	float data[SIZE] = { 0 };	//megfelelő méretü munkatömb
+
+
+								// írási bázis: 0. sor, 0. oszlop (a kimenet NEM kiterjesztett)
+	int wr_base = 0;
+	// olvasási bázis: a kiterjesztett kép bal felső pixele (ez az első konvolúció első bemeneti adata)
+	int rd_base = 0;
+
+	int ext_adress;
+	// Végiglépkedünk a kép sorain
+
+	int row_filter;
+	int col_filter;
+	int i, col, row;
+
+	printf("Median: Pixel steps START\n");
+
+	for (row = 0; row < imgHeight; row++)
+	{
+		// A sorokon belül végiglépkedünk egy sor pixelein
+
+		for (col = 0; col < imgWidth; col++)
+		{
+			printf("Pixel: %dx%d\n", row, col);
+			ext_adress = (row * imgWidthF + col) * 3;
+			wr_base = (row * imgWidth + col) * 3;
+
+			for (i = 0; i < 3; i++)
+			{
+				for (row_filter = 0; row_filter < FILTER_H; row_filter++)
+				{
+
+					for (col_filter = 0; col_filter < FILTER_W; col_filter++)
+					{
+						//tömb feltöltése a szükséges adatokkal
+						data[row_filter * FILTER_W + col_filter] = imgSrcExt[ext_adress + (row_filter*imgWidthF + col_filter) * 3 + i];
+					}
+				}
+
+				odd_even_sort(data);	//rendező alg
+				//oddEvenSort(data, 25);
+				imgDst[wr_base + i] = data[12];	//medián visszaadása
+			}
+		}
+	}
+
+	printf("Median: Pixel steps STOP\n");
+}*/
+
+
+void median_filter(int imgHeight, int imgWidth, int imgHeightF, int imgWidthF, unsigned char *imgSrcExt, unsigned char *imgDst)
+{
+	printf("MEDIAN START!\n");
+
+	unsigned char frame[SIZE] = { 0 };
+
+	for (int row = 0; row < imgHeight; row++)
+	{
+		int wr_base = row * imgWidth * 3;
+		int rd_base = row * imgWidthF * 3;
+
+		for (int col = 0; col < imgWidth; col++)
+		{
+			printf("PIXEL %dx%d\n", row, col);
+			int rd_offset = ((FILTER_W-1)/2 + col) * 3;
+			short fval[3] = { 0,0,0 };
+
+			int pixel_src = rd_base + rd_offset + (FILTER_H-1)/2 * imgWidthF;
+			int filter_src = pixel_src - (FILTER_H - 1) / 2 * imgWidthF;
+
+			printf("Pixel source: %d\nFilter source:%d\n", pixel_src, filter_src);
+
+			for (int rgb = 0; rgb < 3; rgb++)
+			{
+				for (int fy = 0; fy < FILTER_H; fy++)
+				{
+					for (int fx = 0; fx < FILTER_W; fx++)
+					{
+						frame[fx * FILTER_W + fy] = imgSrcExt[filter_src + fx * imgWidthF + fy + rgb];
+					}
+				}
+
+				oddEvenSort(frame, 25);
+				fval[rgb] = frame[12];
+				if (fval[rgb] > 255) fval[rgb] = 255;
+				if (fval[rgb] < 0)   fval[rgb] = 0;
+				*(imgDst + wr_base + rgb) = (unsigned char)(fval[rgb]);
+
+				printf("Pixel src_%d: %d\n", rgb, imgSrcExt[pixel_src+rgb]);
+				printf("Pixel mod_%d: %d\n", rgb, fval[rgb]);
+			}
+	
+			wr_base = wr_base + 3;
+
+		}
+		wr_base = wr_base + 4 * 3;
+
+	}
+
+	printf("MEDIAN STOP!\n");
+}
