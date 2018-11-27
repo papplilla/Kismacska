51ccfc87f0d587a78a112534af9bb742d0e4478d
Strahl Balazs <balazs.strahl@gmail.com>
2018-11-27 10:02:33 +0100

    Fájlok áthelyezve.

--- Hetero_HW_2/_src/func.h
+++
@@ -1,8 +0,0 @@
-void median_filter(int imgHeight, int imgWidth, int imgHeightF,
-	int imgWidthF, unsigned char *imgSrcExt, unsigned char *imgDst);
-
-//void median_filter_avx(int imgHeight, int imgWidth, int imgWidthF,
-//	short *filter, unsigned char *imgSrcExt, unsigned char *imgDst);
-
-void odd_even_sort(float *data);
-void oddEvenSort(unsigned char *arr, int n);
